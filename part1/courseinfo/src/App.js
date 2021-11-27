add23944e284ab3dc4fde2dd2123947a6ac57834
kavish12 <kavishghime90@gmail.com>
2021-11-26 21:21:53 -0800

    exercise 1.1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of component'
+  const exercises3 = 14
+
+  return (
+    <>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total exercise={exercises1 + exercises2 + exercises3} />
+    </>
+  )
+}
+
+export default App
