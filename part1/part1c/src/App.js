bb3b572f2c489ffbeb38b252e8dab50a9d2eae63
kavishghime <kavishghime90@gmail.com>
2021-12-01 01:38:45 -0800

    event handling

--- part1/part1c/src/App.js
+++ part1/part1c/src/App.js
@@ -3,13 +3,20 @@ import React, { useState } from 'react'
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  setTimeout(() => setCounter(counter + 1), 1000)
-
-  console.log('rendering...', counter)
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      {counter}
+      <div>
+        {counter}
+      </div>
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+      <button onClick={setToZero}>
+        zero
+      </button>
     </div>
   )
 }
